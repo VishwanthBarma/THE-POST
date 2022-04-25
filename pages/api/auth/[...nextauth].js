0f5944d58cfaa592e773a9d48d2b708b10d2d780
Barma Vishwanth <vishwanthbsavp@gmail.com>
2022-04-26 02:27:40 +0530

    updated secret

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -10,20 +10,20 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
-  secret: process.env.SECRET,
   pages: {
     signIn: "/auth/signin",
   },
-
+  
   callbacks: {
     async session({session, token, user}){
       session.user.username = session.user.name
-        .split(" ")
-        .join("")
-        .toLocaleLowerCase();
-
+      .split(" ")
+      .join("")
+      .toLocaleLowerCase();
+      
       session.user.uid = token.sub;
       return session;
     }
-  }
+  },
+  secret: process.env.SECRET
 })
