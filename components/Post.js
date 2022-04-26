5c58e88a9dd3ead7a781abb86aec9a462e02fbe4
Barma Vishwanth <72876374+VishwanthBarma@users.noreply.github.com>
2022-04-26 10:37:22 +0530

    Update Post.js
    Updated post comp

--- components/Post.js
+++ components/Post.js
@@ -77,7 +77,7 @@ function Post({post, postId, userid}) {
     }, [saves]);
 
     useEffect(() => {
-        setIsUserPost(posts.findIndex((post) => (post.data().email === session.user?.email)) !== -1);
+        setIsUserPost(posts.findIndex((Upost) => (Upost.data().email === session.user?.email)) !== -1);
     }, [posts]);
 
 
